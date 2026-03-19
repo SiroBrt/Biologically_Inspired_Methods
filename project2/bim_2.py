46ee226361d45177f2fd4286c05fa1809cf55cf9
dubi du <100496683@alumnos.uc3m.es>
2026-03-20 00:14:28 +0100

    ya tenemos casi todo. Faltan el metodo de selección y las ganas de implementarlo

--- project2/bim_2.py
+++ project2/bim_2.py
@@ -12,6 +12,8 @@ from deap import creator
 from deap import gp
 from deap import tools
 
+MUTATION_RATE = 0.05
+POPULATION_SIZE = 100
 
 class Pindividual:
     def __init__(self, expr):
@@ -37,8 +39,11 @@ class Pindividual:
     def copy(self):
         return Pindividual(self.expr)
 
-    # def calculate_fitness(self):
-    #     self.fitness = 0
+    def calculate_fitness(self):
+        y_ = np.array([self.function(x_) for x_ in x])
+        self.fitness = np.mean(np.abs(y_ - y))
+        return self.fitness
+    
 
     def __lt__(self, other):
         if self.fitness == 0:
@@ -52,16 +57,31 @@ class Pindividual:
         expr1, expr2 = gp.cxOnePoint(self.tree, partner.tree)
         return Pindividual(expr1), Pindividual(expr2)
 
-    def punctual_mutation(self, prob):
-        p = random.random()
+    def mut_prune(self):
+        return Pindividual(list(list(gp.mutShrink(self.tree))[0]))
+
+    # keeps structure but changes some leaves and nodes
+    def mut_reroll(self):
+        # change some leaves
+        changes = int(1+MUTATION_RATE*len(list(self.tree))*abs(random.gauss(0,2)))
+        for i in range(changes):
+            tree = gp.mutEphemeral(self.tree,"one")[0]
+        # change some nodes
+        changes = int(1+MUTATION_RATE*len(list(self.tree))*abs(random.gauss(0,2)))
+        for i in range(changes):
+            tree = gp.mutNodeReplacement(tree,pset)[0]
+        return Pindividual(list(tree))
+
+    def phenotypic_dist(self,other):
+        val = np.mean([abs(self.function(i)-other.function(i)) for i in x])
+        return val
 
 
 
+data = pd.read_csv('pi.csv', header=None)
 
-# data = pd.read_csv('pi.csv', header=None)
-#
-# x = data.iloc[1:,0].values
-# y = data.iloc[1:,1].values
+x = data.iloc[1:,0].values
+y = data.iloc[1:,1].values
 #
 # def gauss_legendre(x):
 #   return x / np.log(x)
@@ -76,29 +96,45 @@ class Pindividual:
 # print(f'Fitness Gauss-Legendre: {fitness(gauss_legendre)}')
 # print(f'Fitness Li: {fitness(li)}')
 
+
+min_init_depth = 2
+max_init_depth = 5
+pset = gp.PrimitiveSet("MAIN", 1)
+pset.renameArguments(ARG0="x")
+
 def div(a,b):
+    if b == a:
+        return 1
     if b == 0:
         return np.inf
     return a/b
 
-min_init_depth = 3
-max_init_depth = 3
-pset = gp.PrimitiveSet("MAIN", 1)
-pset.renameArguments(ARG0="x")
-
 pset.addPrimitive(operator.add, 2)
 pset.addPrimitive(operator.sub, 2)
 pset.addPrimitive(operator.mul, 2)
-pset.addPrimitive(operator.pow, 2)
 pset.addPrimitive(div, 2)
+pset.addPrimitive(operator.pow, 2)
 pset.addPrimitive(np.log, 1)
 
-for i in range(10): pset.addTerminal(i)
-pset.addTerminal(np.e)
-pset.addTerminal(np.pi)
+def efimeros():
+    p = random.random()
+    threshold = 0.05 # for constants
+    constants = [np.pi,np.e]
+    num = len(constants)
+    if p<1-threshold:
+        return round(random.uniform(-10, 10),1)
+    for i in range(num):
+        if p<1-threshold*(num-i)/num:
+            return constants[i]
 
+pset.addEphemeralConstant("num",efimeros)
 
-tree1 = Pindividual(gp.genHalfAndHalf(pset, min_=min_init_depth, max_=max_init_depth))
-tree1.print_tree("tree1.png")
-print(tree1)
+random.seed(1)
+population = []
+for i in range(POPULATION_SIZE):
+    population.append(Pindividual(gp.genHalfAndHalf(pset, min_=min_init_depth, max_=max_init_depth)))
+for i in range(10):
+    population[i].print_tree(f"tree{i}.png")
+
+print(population[0].phenotypic_dist(population[2]))
 
